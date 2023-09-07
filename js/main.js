92a06cc33d7a1d738a8b7d8cabb327b29a5a8fbf
Ericdid <ericdidom@gmail.com>
2023-09-07 02:49:51 +0200

    fix.style/add.array

--- js/main.js
+++ js/main.js
@@ -6,6 +6,21 @@ createApp({
   data() {
     return {
       title: "to-do List",
+
+      toDoList: [
+        {
+          task: "task 1",
+          done: false,
+        },
+        {
+          task: "task 2",
+          done: false,
+        },
+        {
+          task: "task 3",
+          done: false,
+        },
+      ],
     };
   },
 }).mount("#app");
